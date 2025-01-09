d6ae0e09018d1a1328322628b7bd9a725046bae1
roney ruiz <roneyruizrojas@gmail.com>
2025-01-08 21:37:29 -0600

    intial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+const root = document.getElementById('root')!
+
+const component = (
+
+);
+
+createRoot(root).render(component);
