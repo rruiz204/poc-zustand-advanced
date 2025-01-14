cc324a5b66f7d2204febeadfe7d58764d705410c
roney ruiz <roneyruizrojas@gmail.com>
2025-01-13 22:17:59 -0600

    feat(client): create task page

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import { Home } from "./pages/Home";
+import { Tasks } from "./pages/Tasks";
 import { Basics } from "./pages/Basics";
 
 export const Router = (): JSX.Element => {
@@ -8,6 +9,7 @@ export const Router = (): JSX.Element => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/tasks" element={<Tasks></Tasks>}></Route>
         <Route path="/basics" element={<Basics></Basics>}></Route>
       </Routes>
     </BrowserRouter>
